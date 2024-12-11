8daf390d377319caa02cb2b051121a09ef4acd4d
EDaley-FlemCollege <122706767+EDaley-FlemCollege@users.noreply.github.com>
2024-12-11 10:52:09 -0500

    Final Submission

--- IDS_Script.py
+++ IDS_Script.py
@@ -4,15 +4,18 @@ from threading import Thread
 import time,sys,subprocess,os
 from scapy.all import Dot11, Dot11Deauth, Dot11Disas, RadioTap, Dot11Elt, sendp, sniff, conf, EAPOL, Dot11EltRSN
 
-if len(sys.argv) < 2:
-	chan = input('Enter Channel: ')
-else :
-	chan=sys.argv[1]
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 WHITELIST = os.path.join(SCRIPT_DIR, 'whitelist.txt')
 
 bssids = ['5A:6D:67:AC:90:90']
+mssid = "eridal"
+
+if len(sys.argv) > 2:
+	mssid = sys.argv[2]
+if len(sys.argv) < 2:
+	chan = input('Enter Channel: ')
+else :
+	chan=sys.argv[1]
 
 subprocess.run("sudo airmon-ng check kill > /dev/null", shell=True, executable="/bin/bash")
 subprocess.run("sudo airmon-ng start wlan0 > /dev/null", shell=True, executable="/bin/bash")
@@ -24,6 +27,7 @@ COUNT_DIS = 0
 COUNT_DEAUTH = 0
 COUNT_AUTH = 0
 COUNT_SPOOF = 0
+STATUS_EVIL = False
 
 s=conf.L2socket(iface='wlan0')
 
@@ -52,6 +56,7 @@ def counter():
 	global COUNT_DEAUTH
 	global COUNT_AUTH
 	global COUNT_SPOOF
+	global STATUS_EVIL
 	bflood_limit = 400
 	oflood_limit = 50
 	while True:
@@ -66,11 +71,14 @@ def counter():
 			print(f"WARNING: AUTHENTICATION FLOOD, {COUNT_AUTH} Frames Captured")
 		if COUNT_SPOOF > 0:
 			print(f"WARNING: {COUNT_SPOOF} Spoofed Frames Captured")
+		if STATUS_EVIL is True:
+			print("WARNING: EVIL TWIN")
 		COUNT_BEACON = 0
 		COUNT_DIS = 0
 		COUNT_DEAUTH = 0
 		COUNT_AUTH = 0
 		COUNT_SPOOF = 0
+		STATUS_EVIL = False
 
 def assoc_check(packet):
 	global COUNT_SPOOF
@@ -89,26 +97,32 @@ def probe_check(packet):
 		COUNT_SPOOF += 1
 	if (packet.addr1.casefold() in read_file(WHITELIST) and not packet.haslayer(Dot11EltRSN)):
                 print(f"WARNING: Evil Twin")
-                #craft_deauth(packet,s)
+                craft_deauth(packet,s)
 
 
 def beacon_check(packet):
 	global COUNT_BEACON
 	global COUNT_SPOOF
+	global STATUS_EVIL
 	ssid = str(packet[Dot11Elt].info)
 	ssid = ssid.split("'")
 	ssid = ssid[1]
 	bssid = packet.addr2
 	if len(ssid) == 0:
 		ssid = "None"
+	if ssid == mssid and not packet.haslayer(Dot11EltRSN):
+                STATUS_EVIL = True
+                craft_deauth(packet,s)
+                COUNT_BEACON += 1
+                return None
 	if bssid.casefold() not in read_file(WHITELIST):
-		#print(f"WARNING: Rogue AP, {ssid}, {bssid}")
-		#craft_deauth(packet,s)
+		print(f"WARNING: Rogue AP, {ssid}, {bssid}")
+		craft_deauth(packet,s)
 		COUNT_BEACON += 1
 		return None
 	if not packet.haslayer(Dot11EltRSN):
-		print(f"WARNING: Evil Twin")
-		#craft_deauth(packet,s)
+		STATUS_EVIL = True
+		craft_deauth(packet,s)
 		COUNT_BEACON += 1
 		return None
 	if ssid == "None":
@@ -137,12 +151,14 @@ def craft_deauth(packet,s):
 		s.send(deauth_frame_client)
 		s.send(deauth_frame_AP)
 
-print(read_file(WHITELIST))
+print("Starting IDS...")
+print(f"IDS Whitelist: {read_file(WHITELIST)}")
 
 counter_thread = Thread(target=counter)
 counter_thread.daemon = True
 counter_thread.start()
 
-print("Starting...")
+print("Started IDS")
+print("-------------------------------------")
 sniff(iface='wlan0',prn=Process_Frame,lfilter=lambda pkt: pkt.haslayer(Dot11), store=0)
 
